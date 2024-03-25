7b687f29fa88c82091b649ce98d4976c76a633d6
User <jmlee@tonesol.com>
2024-03-25 10:39:02 +0900

    0325

---
+++ hellot/src/main/java/com/hana/HellotApplication.java
@@ -0,0 +1,13 @@
+package com.hana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellotApplication.class, args);
+	}
+
+}
