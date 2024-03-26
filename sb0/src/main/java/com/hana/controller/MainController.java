f9022b7bc3ef9b949d8403e4ca7fc8aee4e71549
User <jmlee@tonesol.com>
2024-03-26 16:17:21 +0900

    0326

---
+++ sb0/src/main/java/com/hana/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.hana.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String main(){
+        return "index";
+    }
+
+
+}
