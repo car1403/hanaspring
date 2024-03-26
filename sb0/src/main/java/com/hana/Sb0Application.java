60c78b2ff3f6a3d8ea70e2704400c9ce7d972395
User <jmlee@tonesol.com>
2024-03-26 16:17:30 +0900

    0326

---
+++ sb0/src/main/java/com/hana/Sb0Application.java
@@ -0,0 +1,13 @@
+package com.hana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb0Application.class, args);
+    }
+
+}
---
+++ sb0/src/test/java/com/hana/Sb0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
