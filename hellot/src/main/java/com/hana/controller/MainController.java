743ac929b27f3e58416e462a2e9756ccfc86bbfe
User <jmlee@tonesol.com>
2024-03-26 10:28:09 +0900

    0326

--- hellot/src/main/java/com/hana/controller/MainController.java
+++ hellot/src/main/java/com/hana/controller/MainController.java
@@ -1,12 +1,22 @@
 package com.hana.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
     @RequestMapping("/")
-    public String main(){
+    public String main(Model m){
+        List<String> list = new ArrayList<>();
+        list.add("DATA1");
+        list.add("DATA2");
+        list.add("DATA3");
+        m.addAttribute("data","MyData");
+        m.addAttribute("datas",list);
         return "index";
     }
 }
