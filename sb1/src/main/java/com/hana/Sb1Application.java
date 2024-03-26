ecb65f137264f42b52b0869bae52056bdbeea0b8
User <jmlee@tonesol.com>
2024-03-26 13:21:12 +0900

    0326

---
+++ sb1/src/main/java/com/hana/Sb1Application.java
@@ -0,0 +1,13 @@
+package com.hana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb1Application.class, args);
+	}
+
+}
