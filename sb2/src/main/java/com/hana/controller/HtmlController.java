abba419da62f67b8267dea56c4f526782d4710f3
User <jmlee@tonesol.com>
2024-04-01 15:31:39 +0900

    0401

--- sb2/src/main/java/com/hana/controller/HtmlController.java
+++ sb2/src/main/java/com/hana/controller/HtmlController.java
@@ -4,6 +4,7 @@ import com.hana.app.data.dto.CustDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,14 +36,25 @@ public class HtmlController {
         // Data를 DB에서 조회 한다.
         List<CustDto> list = new ArrayList<>();
         list.add(new CustDto("id01","pwd01","james"));
-        list.add(new CustDto("id01","pwd01","james"));
-        list.add(new CustDto("id01","pwd01","james"));
-        list.add(new CustDto("id01","pwd01","james"));
-        list.add(new CustDto("id01","pwd01","james"));
+        list.add(new CustDto("id02","pwd01","james"));
+        list.add(new CustDto("id03","pwd01","james"));
+        list.add(new CustDto("id04","pwd01","james"));
+        list.add(new CustDto("id05","pwd01","james"));
 
         model.addAttribute("custs", list);
         model.addAttribute("left", dir+"left");
         model.addAttribute("center",dir+"html3");
+        return "index";
+    }
+
+    @RequestMapping("/get")
+    public String get(Model model, @RequestParam("id") String id){
+        // id값을 DB에 조회 한다.
+        CustDto c = CustDto.builder().id(id).pwd("pwdxx").name("james").build();
+
+        model.addAttribute("cust", c);
+        model.addAttribute("left", dir+"left");
+        model.addAttribute("center",dir+"get");
         return "index";
     }
 }
--- sb2/src/main/webapp/views/html/html3.jsp
+++ sb2/src/main/webapp/views/html/html3.jsp
@@ -22,9 +22,9 @@
         <tbody>
             <c:forEach var="c" items="${custs}">
                  <tr>
-                    <td>${c.id}</td>
-                    <td>${c.pwd}</td>
-                    <td>${c.name}</td>
+                     <td><a href="<c:url value="/html/get"/>?id=${c.id}">${c.id}</a></td>
+                     <td>${c.pwd}</td>
+                     <td>${c.name}</td>
                  </tr>
             </c:forEach>
         </tbody>
