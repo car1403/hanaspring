2c6d304df96c73fcefdaf860cd1f6add27c9bea9
User <jmlee@tonesol.com>
2024-04-01 16:02:26 +0900

    0401

---
+++ sb2-th/src/main/java/com/hana/Sb2ThApplication.java
@@ -0,0 +1,13 @@
+package com.hana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb2ThApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb2ThApplication.class, args);
+    }
+
+}
