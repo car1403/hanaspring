57bb031070dfea5b13fbcc90c9badd4148ce62b7
User <jmlee@tonesol.com>
2024-04-03 09:18:21 +0900

    0403

---
+++ sb2/src/test/java/com/hana/addr/SelectOneTests.java
@@ -0,0 +1,37 @@
+package com.hana.addr;
+
+import com.hana.app.data.dto.AddrDto;
+import com.hana.app.service.AddrService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DuplicateKeyException;
+
+import java.sql.SQLException;
+
+@SpringBootTest
+@Slf4j
+class SelectOneTests {
+
+    @Autowired
+    AddrService addrService;
+    @Test
+    void contextLoads() {
+        try {
+            AddrDto addr = null;
+            addr = addrService.get(3);
+            log.info("----------OK----------------");
+        } catch (Exception e) {
+            if(e instanceof SQLException){
+                log.info("----------System Trouble EX0001----------------");
+            }else if(e instanceof DuplicateKeyException){
+                log.info("----------ID Duplicated EX0002----------------");
+            }else{
+                log.info("----------Sorry EX0003----------------");
+            }
+
+        }
+    }
+
+}
