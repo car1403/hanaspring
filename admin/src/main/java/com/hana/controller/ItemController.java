621b6cfd695cb2740ae339a6ffdcf144ead58493
User <jmlee@tonesol.com>
2024-04-05 11:27:23 +0900

    0405

--- admin/src/main/java/com/hana/controller/ItemController.java
+++ admin/src/main/java/com/hana/controller/ItemController.java
@@ -1,16 +1,33 @@
 package com.hana.controller;
 
+import com.hana.app.data.dto.ItemDto;
+import com.hana.app.service.ItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/item")
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemService itemService;
     String dir = "item/";
     @RequestMapping("/get")
-    public String get(Model model){
-        model.addAttribute("center",dir+"get");
+    public String get(Model model) throws Exception {
+        List<ItemDto> list = new ArrayList<>();
+        try {
+            list = itemService.get();
+            model.addAttribute("itemlist",list);
+            model.addAttribute("center",dir+"get");
+        } catch (Exception e) {
+            throw new Exception("EI0001");
+        }
+
         return "index";
     }
 }
