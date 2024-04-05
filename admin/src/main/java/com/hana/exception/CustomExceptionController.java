83d31ee6fcdce00ac06de364b0bd61d0fbfe41ad
User <jmlee@tonesol.com>
2024-04-05 11:13:26 +0900

    0405

--- admin/src/main/java/com/hana/exception/CustomExceptionController.java
+++ admin/src/main/java/com/hana/exception/CustomExceptionController.java
@@ -1,5 +1,6 @@
 package com.hana.exception;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class CustomExceptionController {
     @ExceptionHandler(Exception.class)
     public String except(Exception e, Model model){
+        model.addAttribute("msg", e.getMessage());
+        model.addAttribute("center", "error");
+        return "index";
+    }
+    @ExceptionHandler(DuplicateKeyException.class)
+    public String except2(Exception e, Model model){
         model.addAttribute("msg", e.getMessage());
         model.addAttribute("center", "error");
         return "index";
