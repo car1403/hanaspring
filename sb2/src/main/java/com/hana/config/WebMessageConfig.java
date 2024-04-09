edbcf37ea239ff9119ace283155ef0d20ffc1541
User <jmlee@tonesol.com>
2024-04-09 15:56:08 +0900

    0409

---
+++ sb2/src/main/java/com/hana/config/WebMessageConfig.java
@@ -0,0 +1,72 @@
+package com.hana.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.SimpleLocaleContext;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.i18n.LocaleContextResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class WebMessageConfig implements ApplicationContextAware {
+
+    ApplicationContext context;
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages/message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) {
+        this.context = context;
+    }
+}
+
+// 사용자가 사용하는 브라우져의 사용 언어 확인
+@Configuration
+class LocaleResolve implements LocaleContextResolver {
+
+    @Override
+    public LocaleContext resolveLocaleContext(ServerWebExchange exchange) {
+        //String language = exchange.getRequest().getHeaders().getFirst("Accept-Language");
+        String language = exchange.getRequest().getQueryParams().getFirst("lang");
+        Locale targetLocale = Locale.getDefault();
+        if (language != null && !language.isEmpty()) {
+            targetLocale = Locale.forLanguageTag(language);
+        }
+        return new SimpleLocaleContext(targetLocale);
+    }
+
+    @Override
+    public void setLocaleContext(ServerWebExchange exchange, LocaleContext localeContext) {
+        throw new UnsupportedOperationException("Not Supported");
+    }
+
+}
+
+// 언어펙 적용
+@Configuration
+class MessageByLocale {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
+    private LocaleResolve localeResolver;
+
+    public String getMessage(String code, ServerWebExchange exchange) {
+        LocaleContext localeContext = localeResolver.resolveLocaleContext(exchange);
+        return messageSource.getMessage(code, null, localeContext.getLocale());
+    }
+}
--- sb2/src/main/webapp/views/index.jsp
+++ sb2/src/main/webapp/views/index.jsp
@@ -2,6 +2,8 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <!-- JSTL -->
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -35,7 +37,8 @@
 <body>
 
 <div class="jumbotron text-center" style="margin-bottom:0">
-    <h1>HTML5 & JavaScript</h1>
+    <h1><spring:message code="site.title"  arguments="aa,aa"  /></h1>
+    <h5><spring:message code="site.phone"   /></h5>
 </div>
 <c:choose>
     <c:when test="${sessionScope.id == null}">
