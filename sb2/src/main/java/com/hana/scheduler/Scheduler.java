8270b2b2ac70de082104dbecf245430765295b62
User <jmlee@tonesol.com>
2024-04-09 16:51:29 +0900

    0409

---
+++ sb2/src/main/java/com/hana/scheduler/Scheduler.java
@@ -0,0 +1,25 @@
+package com.hana.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class Scheduler {
+
+    @Scheduled(cron = "*/15 * * * * *")
+    public void cronJobDailyUpdate() {
+        log.info("--------------------------------------------------");
+        log.info("사용자 로그인 정보를 조회 하여 로그를 기록");
+        log.info("--------------------------------------------------");
+    }
+
+    @Scheduled(cron = "1 0 0 1,8,15,22 * *")
+    public void cronJobWeeklyUpdate(){
+
+    }
+
+
+}
---
+++ sb2/src/test/java/com/hana/weather/Test.java
@@ -0,0 +1,16 @@
+package com.hana.weather;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class Test {
+
+    @org.junit.jupiter.api.Test
+    void contextLoads() {
+
+        log.info("----------LOG----------------");
+    }
+
+}
