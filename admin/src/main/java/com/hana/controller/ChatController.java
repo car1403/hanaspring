fd2d6313fcd77627ca4afb5b549c6d8af2395ac8
User <jmlee@tonesol.com>
2024-04-16 13:34:36 +0900

    0415

--- admin/src/main/java/com/hana/controller/ChatController.java
+++ admin/src/main/java/com/hana/controller/ChatController.java
@@ -1,8 +1,10 @@
 package com.hana.controller;
 
 import com.hana.app.data.msg.Msg;
+import com.hana.util.NcpUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -14,14 +16,24 @@ import org.springframework.stereotype.Controller;
 public class ChatController {
 
     final SimpMessagingTemplate template;
-
+    @Value("${app.key.ncp-id}")
+    String ncpId;
+    @Value("${app.key.ncp-secret}")
+    String ncpSecret;
     @MessageMapping("/sendchat") // 특정 Id에게 전송
     public void sendchat(Msg msg, SimpMessageHeaderAccessor headerAccessor) {
         String id = msg.getSendid();
         String target = msg.getReceiveid();
         log.info("-------------------------");
         log.info(msg.toString());
-        template.convertAndSend("/send3/to/"+target,msg);
         template.convertAndSend("/send3/to/"+id,msg);
+
+        String sentiment =
+                NcpUtil.getSentimentDetail(
+                        NcpUtil.getSentiment(ncpId, ncpSecret, msg.getContent1())
+                );
+        msg.setContent1(msg.getContent1()+":"+sentiment);
+        template.convertAndSend("/send3/to/"+target,msg);
+
     }
 }
---
+++ admin/src/main/java/com/hana/util/NcpUtil.java
@@ -0,0 +1,117 @@
+package com.hana.util;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NcpUtil {
+
+    public static String getSentimentDetail(JSONObject jsonObject){
+        String result = "";
+        result = (String) ((JSONObject)jsonObject.get("document")).get("sentiment");
+        return result;
+    }
+
+    public static JSONObject getSentiment(String ncpId, String ncpSecret, String text){
+        JSONParser jsonParser = new JSONParser();
+        JSONObject returnObject = null;
+        try {
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze"; //
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json;UTF-8");
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", ncpId);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", ncpSecret);
+            // post request
+            //  String postParams = "content=" + text;
+            con.setDoOutput(true);
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("content",text);
+
+
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.write(jsonObject.toString().getBytes("UTF-8"));
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 오류 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            returnObject = (JSONObject) jsonParser.parse(response.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return returnObject;
+    }
+
+
+    public static Object getSummary(String ncpId, String ncpSecret, String text){
+        JSONParser jsonParser = new JSONParser();
+        JSONObject returnObject = null;
+        try {
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/text-summary/v1/summarize"; //
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json;UTF-8");
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", ncpId);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", ncpSecret);
+            // post request
+            //  String postParams = "content=" + text;
+            con.setDoOutput(true);
+
+            JSONObject jsonObject = new JSONObject();
+
+            JSONObject documentObject = new JSONObject();
+            documentObject.put("content",text);
+
+            JSONObject optionObject = new JSONObject();
+            optionObject.put("language","ko");
+            optionObject.put("model","news");
+            optionObject.put("summaryCount",2);
+
+            jsonObject.put("document",documentObject);
+            jsonObject.put("option",optionObject);
+
+
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.write(jsonObject.toString().getBytes("UTF-8"));
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 오류 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            returnObject = (JSONObject) jsonParser.parse(response.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return returnObject;
+    }
+}
