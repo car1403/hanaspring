ed123e567b4ba5c1900dc7a71bbef110bc7853e5
User <jmlee@tonesol.com>
2024-04-17 14:32:45 +0900

    0417

---
+++ sb2/src/test/java/com/hana/kakao/KakaoTests1.java
@@ -0,0 +1,66 @@
+package com.hana.kakao;
+
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+@SpringBootTest
+@Slf4j
+class KakaoTests1 {
+    @Test
+    void contextLoads() throws Exception {
+        String address = "https://dapi.kakao.com/v2/local/search/keyword.JSON";
+
+        String param = "query=" + "공공기관"
+                //+ "&category_group_code=" + "FD6"
+                + "&x=" + "37.5606326"
+                + "&y=" + "126.9433486"
+                + "&radius=" + "1000";
+
+        String apiKey = "c93e43d704d5faa1049cf193e5f8decc";	//발급받은 restapi key
+
+
+
+        URL url = new URL(address);  			//접속할 url 설정
+        HttpURLConnection conn;					//httpURLConnection 객체
+        conn = (HttpURLConnection) url.openConnection();	//접속할 url과 네트워크 커넥션을 연다.
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+        conn.setUseCaches(false);
+        conn.setRequestProperty("Authorization", "KakaoAK " + apiKey);	//Property 설정
+
+        OutputStreamWriter ds = new OutputStreamWriter(conn.getOutputStream());
+        ds.write(param);
+        ds.flush();
+        ds.close();
+
+
+        int responseCode = conn.getResponseCode();		//responseCode를 받아옴.
+
+        InputStream inputStream = conn.getInputStream();	//데이터를 받아오기 위한 inputStream
+        BufferedReader br;		//inputStream으로 들어오는 데이터를 읽기 위한 reader
+        String json = null;
+        Charset charset = Charset.forName("UTF-8");
+        if(responseCode == 200) {
+            br = new BufferedReader(new InputStreamReader(inputStream,charset));
+            json = br.readLine();
+            br.close();
+        }
+        else {
+            System.out.println(" ERROR !!! ");
+        }
+
+        inputStream.close();
+        conn.disconnect();
+        System.out.println(json);
+    }
+
+}
